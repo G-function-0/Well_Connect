14751451176801ca78d73fd7d6daa39ea45868a0
Ujjawal Verma <ujjawalverma221@gmail.com>
2026-01-04 22:39:51 +0530

    Implemented refresh tokens

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -5,4 +5,5 @@ import authContoller from "../auth/controller/auth.controller.js"
 
 authRouter.post("/register", authContoller.register);
 authRouter.post("/login", authContoller.login)
+authRouter.post("/refresh", authContoller.refresh)
 export default authRouter;
