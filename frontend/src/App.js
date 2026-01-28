d8cfad908eb8bbda707e81739d7dce29c0247a76
Ujjawal Verma <ujjawalverma221@gmail.com>
2026-01-28 13:04:57 +0530

    Modify app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,8 @@ import Header from './components/layout/Header.jsx';
 import Home from './pages/Home.jsx';
 import Footer from './components/layout/Footer.jsx';
 import LoginPage from './pages/LoginPage.jsx';
-import CoursePlayerPage from './pages/CoursePlayerPage.jsx';
-import CourseDetailPage from './pages/CourseDetailPage.jsx';
-import LiveClassPage from './pages/LiveClassPage.jsx';
-import MyCoursesPage from './pages/MyCoursesPage.jsx';
 import DashboardPage from './pages/DashboardPage.jsx';
 import NutritionPage from './pages/NutritionPage.jsx';
-import ProfilePage from './pages/ProfilePage.jsx';
-import TrainerDashboardpage from './pages/TrainerDashboardpage.jsx';
-import TrainerProfilePage from './pages/TrainerProfilePage.jsx';
 import CoursesPage from './pages/Coursespages.jsx';
 import RegisterPage from './pages/Registerpage.jsx';
 
